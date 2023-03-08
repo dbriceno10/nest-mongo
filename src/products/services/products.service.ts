5961d1694e2dc788894035760e788a8a0a22e2ea
Daniel Briceno <daniel.b100726@gmail.com>
2023-03-08 19:54:49 -0400

    Class 19: Relaciones uno a uno referenciadas

--- src/products/services/products.service.ts
+++ src/products/services/products.service.ts
@@ -58,7 +58,10 @@ export class ProductsService {
   // }
 
   async findOne(id: string) {
-    const product = await this.productModel.findById(id).exec();
+    const product = await this.productModel
+      .findById(id)
+      .populate('brand')
+      .exec();
     if (!product) {
       throw new NotFoundException(`Product #${id} not found`);
     }
