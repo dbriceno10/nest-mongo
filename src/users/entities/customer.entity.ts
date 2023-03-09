5effdfe627cb8a1eb15d3e13e166ac82938fa805
Daniel Briceno <daniel.b100726@gmail.com>
2023-03-08 20:17:37 -0400

    Class 20: Relaciones uno a muchos embebidas

--- src/users/entities/customer.entity.ts
+++ src/users/entities/customer.entity.ts
@@ -1,5 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import { Document, Types } from 'mongoose';
 
 @Schema({ timestamps: true })
 export class Customer extends Document {
@@ -11,6 +11,12 @@ export class Customer extends Document {
 
   @Prop()
   phone: string;
+
+  //supongamos que un costumer tiene skills, y que estas son un array, las guardamos en relacion 1:N embebida
+  @Prop({
+    type: [{ name: { type: String }, color: { type: String } }],
+  })
+  skills: Types.Array<Record<string, any>>;
 }
 
 export const CustomerSchema = SchemaFactory.createForClass(Customer);
